9cc8358c64f10a6999150e45cb7530db95259568
Matthew Mahnke <mahnkem@msoe.edu>
2015-12-06 14:55:58 -0600

    Alphabetize git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,20 +1,20 @@
 #!/bin/sh
+alias ga='git add'
+alias gaa='git add -A'
+alias gb='git branch'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gcam='git commit -a -m'
+alias gcb='git-copy-branch-name'
+alias gco='git checkout'
+alias gcm='git commit -m'
+alias gd='git diff'
 alias gl='git pull --prune'
 alias glg="git log --graph --decorate --oneline --abbrev-commit"
 alias gp='git push origin HEAD'
 alias gpa='git push origin --all'
-alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gb='git branch'
-alias ga='git add'
-alias gaa='git add -A'
-alias gcm='git commit -m'
-alias gcam='git commit -a -m'
-alias gs='git status -sb'
-alias gcb='git-copy-branch-name'
 alias gpr='gp && open-pr'
+alias gs='git status -sb'
 
 gi() {
   curl -s "https://www.gitignore.io/api/$*";
