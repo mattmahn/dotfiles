3f5a2b6399ae38b75888fd7923b68149ad82544d
Matthew Mahnke <mahnkem@msoe.edu>
2017-02-24 20:39:01 -0600

    ctags

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if test "$(which pacman)"; then
-  sudo pacman -S--noconfirm --needed gvim
+  sudo pacman -S--noconfirm --needed gvim ctags
 fi
 
 vim +PluginInstall! +qall >/tmp/vim-plugins.log 2>&1 || true
