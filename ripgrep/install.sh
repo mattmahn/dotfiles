b7540c72aa9ffbc4cbe2076461255404db1a7a48
Matthew Mahnke <mmahnke18@gmail.com>
2018-02-03 13:11:21 -0600

    Switch macOS ripgrep to optimized binary

--- ripgrep/install.sh
+++ ripgrep/install.sh
@@ -10,5 +10,7 @@ elif test "$(which yum)"; then  # RHEL/CentOS 7
   yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlgeorge/ripgrep/repo/epel-7/carlgeorge-ripgrep-epel-7.repo
   yum install ripgrep
 elif test "$(which brew)"; then  # Darwin
-  brew install ripgrep
+  # this has SIMD and other optimizations
+  brew tap burntsushi/ripgrep https://github.com/BurntSushi/ripgrep.git
+  brew install burntsushi/ripgrep/ripgrep-bin
 fi
