51515c30f3f8b377c45cc318cca652e42246b445
Matthew Mahnke <mmahnke18@gmail.com>
2018-02-20 16:12:15 -0600

    Update antibody ?

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 if which brew >/dev/null 2>&1; then
   brew untap -q getantibody/homebrew-antibody || true
-  brew tap -q getantibody/homebrew-antibody
-  brew install antibody
+  brew untap -q getantibody/tap/antibody || true
+  brew tap -q getantibody/tap/antibody
+  brew install getantibody/tap/antibody
 else
   curl -sL https://git.io/vwMNi | sh -s
 fi
