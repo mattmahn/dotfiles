d4a1e3a3f5471947c1939f27cf967ede799daf77
Matthew Mahnke <mmahnke18@gmail.com>
2018-05-23 14:53:31 -0500

    That's just how I roll

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,5 +1,6 @@
 #!/bin/zsh
-#add each topic folder to fpath so that they can add functions and completion scripts
+fpath=("$HOME/.zfunc" "$HOME/.zsh/completion" $fpath)
+# add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ($DOTFILES/*) if [ -d "$topic_folder" ]; then
-  fpath=($topic_folder "$HOME/.zfunc" $fpath)
+  fpath=($topic_folder $fpath)
 fi
