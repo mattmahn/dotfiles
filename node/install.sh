470cfa8432253de65ee46c45d51a363907d31be1
Matthew Mahnke <mmahnke18@gmail.com>
2018-06-18 08:46:47 -0500

    Switch to using `n` for Node version mgmt

--- node/install.sh
+++ node/install.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-if test "$(which pacman)"; then
-  sudo pacman -S --noconfirm --needed nodejs npm
-elif [ "$(uname -s)" = "Darwin" ]; then
-  brew install nvm
-fi
 
-if command -v nvm >/dev/null 2>&1; then
-  nvm install node
-  nvm use node
-fi
+curl -L 'https://git.io/n-install' | bash
--- node/path.zsh
+++ node/path.zsh
@@ -2,7 +2,9 @@
 
 [[ -d "$HOME/.npm/bin" ]] && export PATH="$PATH:$HOME/.npm/bin"
 
-if [[ "$(uname -s)" = "Darwin" ]]; then
+export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
+
+if command -v nvm >/dev/null 2>&1; then
   nvm_install_dir="/usr/local/Cellar/nvm"
   latest_nvm_version="$(/bin/ls $nvm_install_dir | sort -rn | head -n 1)"
   . "$nvm_install_dir/$latest_nvm_version/nvm.sh"
