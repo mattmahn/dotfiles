c318e96b280315aad50826fa8c5b37fabf409e61
Matt Mahnke <mattmahn@users.noreply.github.com>
2019-10-01 08:54:13 -0500

    Add asdf-vm

---
+++ asdf/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+asdf_version='v0.7.4'
+asdf_dir="${HOME}/.asdf"
+
+if [ ! -d "$asdf_dir" ]; then
+  git clone https://github.com/asdf-vm/asdf.git "$asdf_dir" --branch "$asdf_version"
+fi
