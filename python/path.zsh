38749ef7fb55630a9de07e56c84e096f8b92fca5
Matt Mahnke <mattmahn@users.noreply.github.com>
2019-10-01 08:30:39 -0500

    python: asdf-ing

--- python/path.zsh
+++ python/path.zsh
@@ -1,31 +1,7 @@
 #!/bin/sh
 
-__pyenv_started=0
-
-__pyenv_init() {
-  test $__pyenv_started = 0 && {
-    eval "$(command pyenv init -)"
-    __pyenv_started=1
-  }
-}
-
-pyenv() {
-  __pyenv_init
-  command pyenv "$@"
-}
-
-python() {
-  __pyenv_init
-  command python "$@"
-}
-
-pip() {
-  __pyenv_init
-  command pip "$@"
-}
-
 # add pip-install location to PATH
-if [ "$(uname)" = "Darwin" ]; then
+if [ "$(uname)" = "Darwin" ] && [ -d ${HOME}/Library/Python/* ]; then
   for version in ${HOME}/Library/Python/*; do
     export PATH="${PATH}:${version}/bin"
   done
