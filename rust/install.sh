523c64b8068a19593f5285d16ed45208549ca7f9
Matt Mahnke <mattmahn@users.noreply.github.com>
2019-10-07 15:32:54 -0500

    fix(cargo): don't reinstall rustup if already installed

--- rust/install.sh
+++ rust/install.sh
@@ -3,5 +3,7 @@
 if command -v pacman >/dev/null 2>&1; then
   sudo pacman -S --noconfirm --needed rustup
 else
-  curl -LsSf https://sh.rustup.rs | sh
+  if ! command -v cargo >/dev/null 2>&1; then  # don't reinstall if already installed
+    curl -LsSf https://sh.rustup.rs | sh
+  fi
 fi
