7f4adaf892260ea726383df366691f7b30ffae9c
Matthew Mahnke <mmahnke18@gmail.com>
2021-05-15 17:12:13 -0500

    TODO: write meaningful commit message

--- editorconfig/install.sh
+++ editorconfig/install.sh
@@ -4,7 +4,7 @@
 if test "$(which pacman)"; then  # Arch
   sudo pacman -S --noconfirm --needed editorconfig-core-c
 elif test "$(which apt-get)"; then  # Fedora 24+
-  sudo apt-get install editoconfig
+  sudo apt-get install editorconfig
 elif test "$(which brew)"; then  # Darwin
   brew install editorconfig
 fi
