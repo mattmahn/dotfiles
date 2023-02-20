3c2c1728a44970ac7cf93700147b23ebda34063c
Matthew Mahnke <mmahnke18@gmail.com>
2023-02-20 17:27:27 -0600

    So my boss wanted this button ...

---
+++ macos/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+if if-command brew; then
+  brew install coreutils
+fi
---
+++ node/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+npm config --global set prefix="${HOME}/.npm"
+npm config --global set sign-git-tag=true
